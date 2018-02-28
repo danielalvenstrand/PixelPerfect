9808fc6ba187237b7f212956b423d874f50ce8bd
Daniel Älvenstrand <daniel.alvenstrand@capgemini.com>
2018-02-28 15:02:40 +0100

    Updated Component class documentation.

--- src/core/component.js
+++ src/core/component.js
@@ -1,4 +1,8 @@
 export class Component {
+    /**
+     * The Component super class extends classes with a DOM element reference.
+     * @param element - the element reference in the DOM.
+     */
     constructor(element) {
         this.elementRef = element;
     }
